e1c6276a868dc4933fb7cdc769e8a89c16a0c3a6
nhlanhlatsolo11-commits <nhlanhla.tsolo11@gmail.com>
2026-09-06 08:43:18 +0200

    Commit backend URL to app.config.js (set expo.extra.apiUrl)

--- app.config.js
+++ app.config.js
@@ -2,14 +2,14 @@ const appJson = require('./app.json');
 
 appJson.expo = appJson.expo || {};
 appJson.expo.android = appJson.expo.android || {};
-// Set your Android package id here (reverse-DNS)
+// Android package id (reverse-DNS)
 appJson.expo.android.package = 'com.funda.ai';
 // Ensure a versionCode exists for Android
 appJson.expo.android.versionCode = appJson.expo.android.versionCode || 1;
 
 // Use extra for runtime configuration such as your backend URL
 appJson.expo.extra = appJson.expo.extra || {};
-// You can override API URL at build time by setting API_URL env variable in EAS
-appJson.expo.extra.apiUrl = process.env.API_URL || 'https://REPLACE_WITH_BACKEND_URL';
+// Backend URL committed by request
+appJson.expo.extra.apiUrl = 'https://funda-backend.onrender.com';
 
 module.exports = appJson;
